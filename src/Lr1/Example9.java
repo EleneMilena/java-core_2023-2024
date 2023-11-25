e6f45bf4e6364b655415ba862d685ef8a7e56ef7
EleneMilena <e-valieva@mail.ru>
2023-11-25 14:36:20 +0500

    1

--- src/Lr1/Example9.java
+++ src/Lr1/Example9.java
@@ -7,30 +7,12 @@ public class Example9 {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите месяц");
         int month = in.nextInt();
-        if (month == 1){
+        if (month == 1 || month == 3 ||month == 5 || month == 7 || month == 8 || month == 10  || month == 12){
             System.out.println("31 день");
-        } else if (month == 2){
-            System.out.println("30 день");
-        } else if (month == 3) {
+        } else if (month == 2) {
             System.out.println("29 день");
-        } else if (month == 4) {
+        } else if (month == 4 || month == 6 || month == 9 || month == 11) {
             System.out.println("30 день");
-        } else if (month == 5) {
-            System.out.println("31 день");
-        } else if (month == 6) {
-            System.out.println("30 день");
-        } else if (month == 7) {
-            System.out.println("31 день");
-        } else if (month == 8) {
-            System.out.println("31 день");
-        } else if (month == 9) {
-            System.out.println("30 день");
-        } else if (month == 10) {
-            System.out.println("31 день");
-        } else if (month == 11) {
-            System.out.println("30 день");
-        } else if (month == 12) {
-            System.out.println("31 день");
         } else {
             System.out.println("Не верное значение месяца");
         }
